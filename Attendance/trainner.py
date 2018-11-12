7440c98b0dfa60211aef39ac1bbbc58605dc768f
Payanshi <32511913+Payanshi@users.noreply.github.com>
2018-11-12 07:47:14 -0800

    Add files via upload

---
+++ Attendance/trainner.py
@@ -0,0 +1,30 @@
+import cv2, os
+
+# Import numpy for matrix calculation
+import numpy as np
+
+# Import Python Image Library (PIL)
+from PIL import Image
+
+# Create Local Binary Patterns Histograms for face recognization
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Using prebuilt frontal face training model, for face detection
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+# Create method to get the images and label data
+def getImagesAndLabels(path):
+
+    # Get all file path
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)] 
+    
+    # Initialize empty face sample
+    faceSamples=[]
+    
+    # Initialize empty id
+    ids = []
+
+    return faceSamples,ids
+
+cv2.destroyAllWindows()
+
