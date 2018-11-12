b6df87aa522817940d18a2f15861a555325f898e
Javin Juneja <32516397+starboi97@users.noreply.github.com>
2018-11-12 19:17:36 +0530

    Add files via upload

---
+++ faceDet.py
@@ -0,0 +1,20 @@
+
+import cv2
+face_cascade=cv2.CascadeClassifier('Haarcacade_frontalface_default.xml')
+eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap=cv2.VideoCapture(0)
+while 1:
+    rt, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+        cv2.imshow('img',img)
+    k=cv2.waitKey(30) & 0xFF
+    if k== 27:
+        break
+
+cap.realease()
+cv2.destroyAllWindows()
